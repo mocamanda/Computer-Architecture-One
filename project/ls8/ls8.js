4a02eab4c308f698d2abf06f1c36bf5b456bdd59
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-06-12 17:17:42 -0700

    Unhardcode machine code to make it User-Friendly

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const RAM = require('./ram');
 const CPU = require('./cpu');
 
@@ -7,31 +9,53 @@ const CPU = require('./cpu');
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
-
+    const program = [];
+    const argv = process.argv;
+    if (argv.length !== 3) {
+      console.error('usage: node ls8 [filename]');
+      process.exit(1);
+    }
     // Hardcoded program to print the number 8 on the console
 
-    const program = [ // print8.ls8
-        // "10011001", // LDI R0,8  Store 8 into R0
-        // "00000000",
-        // "00001000",
-        // "01000011", // PRN R0    Print the value in R0
-        // "00000000",
-        // "00000001"  // HLT       Halt and quit
-    // # mult.ls8
+    const filename = argv[2];
+    const filedata = fs.readFileSync(filename, 'utf8');
+    const lines = filedata.trim().split(/[\r\n]+/g);
+  
+    for (let line of lines) {
+      const cmt = line.indexOf('#');
+      if (cmt !== -1) {
+        line = line.substr(0, cmt);
+      }
+      if (line === '') {
+        continue;
+      }
+      line = line.trim();
+      program.push(line);
+    }
 
-        "10011001", //# LDI R0,8
-        "00000000",
-        "00001000",
-        "10011001", //# LDI R1,9
-        "00000001",
-        "00001001",
-        "10101010", //# MUL R0,R1 <---
-        "00000000",
-        "00000001",
-        "01000011", //# PRN R0
-        "00000000",
-        "00000001", //# HLT
-    ];
+    // const program = [
+    // // print8.ls8
+    //     // "10011001", // LDI R0,8  Store 8 into R0
+    //     // "00000000",
+    //     // "00001000",
+    //     // "01000011", // PRN R0    Print the value in R0
+    //     // "00000000",
+    //     // "00000001"  // HLT       Halt and quit
+    
+    // // # mult.ls8
+    //     "10011001", //# LDI R0,8
+    //     "00000000",
+    //     "00001000",
+    //     "10011001", //# LDI R1,9
+    //     "00000001",
+    //     "00001001",
+    //     "10101010", //# MUL R0,R1 <---
+    //     "00000000",
+    //     "00000001",
+    //     "01000011", //# PRN R0
+    //     "00000000",
+    //     "00000001", //# HLT
+    // ];
 
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
